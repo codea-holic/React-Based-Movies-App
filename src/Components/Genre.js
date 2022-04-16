471594e2f9b5be75b790f5747be8b189ba0aa7c2
Ganesh Singh <krganesh1611@gmail.com>
2022-04-16 21:23:54 +0530

    Initial Commit

---
+++ src/Components/Genre.js
@@ -0,0 +1,8 @@
+import React from 'react'
+function Genre(){
+    return(
+        <div>Genre</div>
+    )
+}
+
+export default Genre;
---
+++ src/Components/Movie.js
@@ -0,0 +1,8 @@
+import React from 'react'
+function Movie(){
+    return(
+        <div>Movie</div>
+    )
+}
+
+export default Movie;
